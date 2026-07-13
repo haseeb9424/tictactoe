9bf9bd57942bdf873d03f008f05a8b533876a8ef
haseeb9424 <haseeb9424@gmail.com>
2026-07-13 20:07:04 +0500

    Add files via upload

--- control.js
+++ control.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+const consoleBtn = document.getElementById('console-btn');
+
+    // Simple event listener that triggers the console.log
+    consoleBtn.addEventListener('click', () => {
+        console.count("Counter Value");
+    });
